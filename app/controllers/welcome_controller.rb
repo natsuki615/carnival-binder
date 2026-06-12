7acae1cee86170ad09e2889799288407719ca1ec
natsuki615 <carinazheng949@gmail.com>
2026-06-12 20:05:19 +0000

    feat: urgent announcement widget on landing page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,5 +4,6 @@ class WelcomeController < ApplicationController
   def index
     @notes = Note.accessible_by(Current.ability).unhidden
     @note = Note.new
+    @urgents = Urgent.active.unhidden.ordered_by_created_at if Current.user&.scc?
   end
 end
