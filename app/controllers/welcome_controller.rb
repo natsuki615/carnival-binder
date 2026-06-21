ebd4490286340ad41fddc8ec334a2d673ade775f
natsuki615 <carinazheng949@gmail.com>
2026-06-21 21:29:09 +0000

    feat: structural/electrical queue preview widget

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,5 +7,9 @@ class WelcomeController < ApplicationController
     return unless Current.user&.scc?
 
     @urgents = Urgent.active.unhidden.ordered_by_created_at
+    @electrical_queue =
+      OrganizationTimelineEntry.electrical.current.includes(:organization)
+    @structural_queue =
+      OrganizationTimelineEntry.structural.current.includes(:organization)
   end
 end
